6f30b0f0b249a0e128623488312ecfb5cc0640b4
Thomas Johansen <thomasjo@gmail.com>
2019-04-01 13:53:52 +0200

    Make script logic more robust

--- docs/prepare-authentication.sh
+++ docs/prepare-authentication.sh
@@ -17,14 +17,27 @@ pub_file="${key_file}.pub"
 
 mkdir -p "${ssh_root}"
 
-if [ ! -f "${key_file}" ]; then
+if [ ! -f "${key_file}" ] && [ ! -f "${pub_file}" ]; then
   ssh-keygen -t rsa -f "${key_file}" -C "${username}@uit.no" -N "" -q
   printf "Created new SSH key pair\n  %s\n  %s\n" $key_file $pub_file
+
+  fingerprint="$(ssh-keygen -lf "${key_file}" | cut -d " " -f 2)"
+  if ! ssh-add -l | grep -q "${fingerprint}"; then
+    sdd-add "${key_file}" >/dev/null
+    printf "Added identity to authentication agent\n  %s\n" $fingerprint
+  fi
+else
+  printf "Using existing SSH key pair\n  %s\n  %s\n" $key_file $pub_file
 fi
 
-kubectl create secret generic ssh-keys \
-  --from-file="${pub_file}" \
-  --namespace="${username}" \
-  --dry-run --output=yaml | kubectl apply --filename=- \
-  >/dev/null 2>&1
-printf "Created k8s secret containing public key\n  %s\n" ${pub_file}
+if kubectl auth can-i create secret -qn "${username}"; then
+  kubectl create secret generic ssh-keys \
+    --from-file="${pub_file}" \
+    --namespace="${username}" \
+    --dry-run --output=yaml | kubectl apply --filename=- \
+    >/dev/null
+  printf "Created k8s secret containing public key\n  %s\n" ${pub_file}
+else
+  printf "Unable to create k8s secret...\n" >&2
+  exit 1
+fi
